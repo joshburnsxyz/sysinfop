511748f713956951c4276edd96748d07e5d73f7c
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-12-14 14:16:28 +1100

    get ascii text for platform logo basic on results of platform.system

--- src/sysinfop/sysinfo.py
+++ src/sysinfop/sysinfo.py
@@ -4,6 +4,17 @@ import re
 import uuid
 import psutil
 
+icon_map = {
+    "Windows": "windows-logo-here",
+    "Linux": "tux-logo-here",
+    "Darwin": "macos-logo-here",
+    "Java": "java-logo-here"
+}
+
+def getASCII(platf):
+    icon = icon_map[platf]
+    return icon
+
 def getSystemInfo():
     try:
         info={}
@@ -16,6 +27,7 @@ def getSystemInfo():
         info['mac-address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
         info['processor']=platform.processor()
         info['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
+        info['
         return info
     except Exception as e:
         print(f"[red]ERROR: {e}[/red]")
