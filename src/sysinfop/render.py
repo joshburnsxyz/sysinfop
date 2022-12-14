c1b4f9b47c9b3cbdfb01d7831dc4cf0c20379946
Josh Burns <joshyburnss@gmail.com>
2022-12-15 09:34:03 +1100

    fix render height

--- src/sysinfop/render.py
+++ src/sysinfop/render.py
@@ -14,5 +14,5 @@ r = data["ram"]
 
 layout = Panel(f"[bold]{h}[/bold]\n* IP: {i}\n* OS: {p}\n* CPU: {P}\n* RAM: {r}",
                expand=False,
-               height=10,
+               height=7,
                box=box)
