fc63d27f60fb129278e577e99c432373ead611ea
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-12-14 14:19:16 +1100

    remove bottom half of output

--- src/sysinfop/render.py
+++ src/sysinfop/render.py
@@ -5,19 +5,10 @@ from .sysinfo import getSystemInfo
 layout = Layout()
 data = getSystemInfo()
 banner = Layout(name=f"{data['hostname']}")
-lower = Layout(name="lower")
 
 layout.split_column(
-    banner,
-    lower
+    banner
 )
 
 # Top banner, basic info
 banner.update(Panel(f"* {data["hostname"]}\n* IP: {data["ip-address"]}\n* OS: {data["platform"]}\n* CPU: {data["processor"]}", title=data["hostname"])
-
-layout["lower"].split_row(
-    Layout(name="left"),
-    Layout(name="right"),
-)
-
-lower["left"].update(data["logo"])
