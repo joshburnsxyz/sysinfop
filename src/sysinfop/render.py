9f2ef3a3397083d36faeae2bf056ed37aaa951c4
Josh Burns <joshyburnss@gmail.com>
2022-12-14 21:55:13 +1100

    render invisible border

--- src/sysinfop/render.py
+++ src/sysinfop/render.py
@@ -1,19 +1,26 @@
 from rich.layout import Layout
 from rich.panel import Panel
+from rich import box
 from .sysinfo import getSystemInfo
 
 layout = Layout()
 data = getSystemInfo()
-banner = Layout(name=f"{data['hostname']}")
-logo = Layout(name=f"{data['hostname']}")
+banner = Layout(name=f"{data['hostname']}",ratio=4)
+logo = Layout(name=f"{data['platform']}",ratio=1)
+box = box.MINIMAL
 
-layout.split_column(
-    banner,
-    logo
+layout.split_row(
+    logo,
+    banner
 )
 
 # System info section
-banner.update(Panel(f"* {data["hostname"]}\n* IP: {data["ip-address"]}\n* OS: {data["platform"]}\n* CPU: {data["processor"]}\n* RAM: {data["ram"]}", title=data["hostname"]))
+h = data["hostname"]
+i = data["ip-address"]
+p = data["platform"]
+P = data["processor"]
+r = data["ram"]
+banner.update(Panel(f"* {h}\n* IP: {i}\n* OS: {p}\n* CPU: {P}\n* RAM: {r}", title=data["hostname"], expand=False, height=10, box=box))
 
 # Logo Section
-logo.update(Panel(f"{data["logo"]}")
+logo.update(Panel(f"{data['logo']}", expand=False, title=data["platform"], height=10, box=box))
