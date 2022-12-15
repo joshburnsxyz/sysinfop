610f815831618f9280db725339f9752037fc5110
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-12-16 07:12:16 +1100

    Iterator loop over a list of tuples to generate the output

--- src/sysinfop/render.py
+++ src/sysinfop/render.py
@@ -5,14 +5,33 @@ from .sysinfo import getSystemInfo
 data = getSystemInfo()
 box = box.MINIMAL
 
-# System info section
-h = data["hostname"]
-i = data["ip-address"]
-p = data["platform"]
-P = data["processor"]
-r = data["ram"]
+banner = f"[bold]{data["hostname"]}[/bold]"
 
-layout = Panel(f"[bold]{h}[/bold]\n* IP: {i}\n* OS: {p}\n* CPU: {P}\n* RAM: {r}",
+def render_item(label,data):
+  return f"[bold]{label}[/label]: {data}"
+
+def render_string(data):
+  str = ""
+  
+  # Loop over list of tuples and pass each tuple to
+  # the render_item() function. Join the results together
+  # and return the resulting string.
+  for d in data:
+    s = str + f"* {render_item(d[0], d[1])}\n"
+    str = s
+  return s
+
+# format_template = f"{banner}\n* {render_item("IP", i)}\n* {render_item("OS", p)}\n* {render_item("CPU", P)}\n* {render_item("RAM", r)}\n* {render_item()}"
+format_template = render_string([
+  ("IP", data["ip-address"]),
+  ("OS", data["platform"]),
+  ("CPU", data["processor"]),
+  ("RAM", data["ram"]),
+  ("MAC", data["mac-address"]),
+  ("ARCH", data["architecture"]),
+])
+
+layout = Panel(format_template,
                expand=False,
                height=7,
                box=box)
